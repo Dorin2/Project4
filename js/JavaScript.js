04ed4c931e68e77435701b8b7ad160839e808647
Dorin <dorin.ghetus98@gmail.com>
2022-06-10 12:16:35 +0300

    commit2

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,10 +1,14 @@
 const button = document.querySelector('.form_button-submit'),
-      form = document.querySelector('.form'),
+      form1 = document.querySelector('.form-section1'),
+      form2 = document.querySelector('.form-section2'),
+      form3 = document.querySelector('.form-section3'),
       number = document.querySelectorAll('.modal_number'),
       steps = document.querySelectorAll('.modal_steps-span');
 
-button.addEventListener('click',()=>{
-    form.classList.add('active');
-    form.classList.remove('disable');
+button.addEventListener('click',()=> {
+    form1.classList.remove('section-show');
+    form1.classList.add('section-hide');
+    form2.classList.remove('section-hide');
+    form2.classList.add('section-show');
 });
 
